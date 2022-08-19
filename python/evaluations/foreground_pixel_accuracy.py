11809fb00ea0992987499248245d443991b70589
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-08-19 10:30:29 +0200

    add fpa f1 + precision + recall calculations

--- python/evaluations/foreground_pixel_accuracy.py
+++ python/evaluations/foreground_pixel_accuracy.py
@@ -86,3 +86,97 @@ def foreground_pixel_accuracy(doc_gt: Document, revision_prediction: Revision, i
             total_cells_viewed += 1
 
     return foreground_pixel_accuracy_total_value / total_cells_viewed
+
+
+def fpa_precision(threshold: float, doc_gt: Document, revision_prediction: Revision, image_filepath: str) -> float:
+    """
+    precision = true positives / (true positives + false positives)
+    precision = (number of cells with fpa bigger than threshold) /
+    [("") + (cells from prediction which don't find cells from the gt with fpa > threshold)]
+
+    :param threshold:
+    :param doc_gt:
+    :param revision_prediction:
+    :param image_filepath:
+    :return:
+    """
+
+    tables_gt: List[Table] = [x for x in doc_gt.objects() if isinstance(x, Table)]
+    tables_prediction: List[Table] = [x for x in revision_prediction.objects if isinstance(x, Table)]
+    image: Image = Image.open(image_filepath)
+
+    matched_tables: Dict[Table, Table] = utility.match_tables(tables_gt, tables_prediction)
+
+    true_positives: int = 0
+    false_positives: int = 0
+
+    for table_gt, table_prediction in matched_tables.items():
+        cells_prediction: List[Cell] = table_prediction.cells if table_prediction is not None else []
+        cells_gt: List[Cell] = table_gt.cells
+
+        cell_gt: Cell
+        for cell_gt in cells_gt:
+            fpa: float = _foreground_pixel_accuracy_for_single_cell(cell_gt, cells_prediction, image)
+            if fpa >= threshold:
+                true_positives += 1
+
+        for cell_prediction in cells_prediction:
+            fpa: float = _foreground_pixel_accuracy_for_single_cell(cell_prediction, cells_gt, image)
+            if fpa < threshold:
+                false_positives += 1
+
+    return true_positives / (true_positives + false_positives)
+
+
+def fpa_recall(threshold: float, doc_gt: Document, revision_prediction: Revision, image_filepath: str):
+    """
+    recall = true positives / (true positives + false negatives)
+    recall = (number of cells with fpa bigger than threshold) /
+    [("") + (cells from gt which don't find cell from prediction with fpa > threshold)]
+
+    :param threshold:
+    :param doc_gt:
+    :param revision_prediction:
+    :param image_filepath:
+    :return:
+    """
+
+    tables_gt: List[Table] = [x for x in doc_gt.objects() if isinstance(x, Table)]
+    tables_prediction: List[Table] = [x for x in revision_prediction.objects if isinstance(x, Table)]
+    image: Image = Image.open(image_filepath)
+
+    matched_tables: Dict[Table, Table] = utility.match_tables(tables_gt, tables_prediction)
+
+    true_positives: int = 0
+    false_negatives: int = 0
+
+    for table_gt, table_prediction in matched_tables.items():
+        cells_prediction: List[Cell] = table_prediction.cells if table_prediction is not None else []
+        cells_gt: List[Cell] = table_gt.cells
+
+        cell_gt: Cell
+        for cell_gt in cells_gt:
+            fpa: float = _foreground_pixel_accuracy_for_single_cell(cell_gt, cells_prediction, image)
+            if fpa >= threshold:
+                true_positives += 1
+            else:
+                false_negatives += 1
+
+    return true_positives / (true_positives + false_negatives)
+
+
+def fpa_f1_score(threshold: float, doc_gt: Document, revision_prediction: Revision, image_filepath: str) -> float:
+    """
+    f1_score = 2*[(precision*recall)/(precision + recall)]
+
+    :param threshold:
+    :param doc_gt:
+    :param revision_prediction:
+    :param image_filepath:
+    :return:
+    """
+
+    precision: float = fpa_precision(threshold, doc_gt, revision_prediction, image_filepath)
+    recall: float = fpa_recall(threshold, doc_gt, revision_prediction, image_filepath)
+
+    return float(2 * ((precision * recall) / (precision + recall)))
