92bd06dc37af73f1213e5588b108417a15243104
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-08-19 11:55:10 +0200

    add table matching to iou calculation for tables

--- python/evaluations/iou.py
+++ python/evaluations/iou.py
@@ -1,21 +1,22 @@
 from docrecjson.elements import Document, PolygonRegion, Cell, Table
+from evaluations import utility
 from shapely.geometry import Polygon
 from loguru import logger
 
 # todo check issues with shapely-speed ->
 #  https://stackoverflow.com/questions/14697442/faster-way-of-polygon-intersection-with-shapely
-from typing import List
+from typing import List, Dict
 
 
 def iou_f1_score_with_threshold(threshold: float, tables_gt: List[Table],
                                 tables_prediction: List[Table]) -> float:
     # Go through each Ground Truth Cell -> find if IOU of this gt cell with some prediction cell is bigger than threshold
-    if len(tables_gt) > 1 or len(tables_prediction) > 1:
-        # todo resolve this issue
-        # create list of polygon regions for each table -> match them against each other?
-        # -> how to match both tables against each other?
-        # -> maybe with a certainity on how much they resemble?
-        raise RuntimeError("Specified table list of length longer than 1. This is currently not supported.")
+    # if len(tables_gt) > 1 or len(tables_prediction) > 1:
+    # todo resolve this issue
+    # create list of polygon regions for each table -> match them against each other?
+    # -> how to match both tables against each other?
+    # -> maybe with a certainity on how much they resemble?
+    # raise RuntimeError("Specified table list of length longer than 1. This is currently not supported.")
 
     if len(tables_prediction) == 0:
         # no table was detected
@@ -28,8 +29,8 @@ def iou_f1_score_with_threshold(threshold: float, tables_gt: List[Table],
     false_negatives: int = 0
     false_positives: int = 0
     for cell in table_gt.cells:
-        iou: float = _iou_for_single_cell(cell, table_prediction.cells)
-        if iou >= threshold:
+        iogt: float = _iogt_for_single_cell(cell, table_prediction.cells)
+        if iogt >= threshold:
             true_positives += 1
         else:
             false_negatives += 1
@@ -57,7 +58,6 @@ def intersection_over_union(doc_gt: Document = None, doc_prediction: Document = 
                             cells_gt: List[Cell] = None, cells_prediction: List[Cell] = None,
                             tables_gt: List[Table] = None, tables_prediction: List[Table] = None) -> float:
     if doc_gt is not None and doc_prediction is not None:
-        # todo support multiple revisions here? -> return dict with revision + IoU value
         polygon_content_gt = [x for x in doc_gt.content if isinstance(x, PolygonRegion)]
         polygon_content_prediction = [x for x in doc_prediction.content if isinstance(x, PolygonRegion)]
         return _intersection_over_union_polygon_region(polygon_content_gt, polygon_content_prediction)
@@ -66,22 +66,22 @@ def intersection_over_union(doc_gt: Document = None, doc_prediction: Document = 
         polygon_content_prediction = [x.bounding_box for x in cells_prediction]
         return _intersection_over_union_polygon_region(polygon_content_gt, polygon_content_prediction)
     elif tables_gt is not None and tables_prediction is not None:
-        if len(tables_gt) > 1 or len(tables_prediction) > 1:
-            # todo resolve this issue
-            # create list of polygon regions for each table -> match them against each other?
-            # -> how to match both tables against each other?
-            # -> maybe with a certainity on how much they resemble?
-            raise RuntimeError("Specified table list of length longer than 1. This is currently not supported.")
-        elif len(tables_gt) == 1 and len(tables_prediction) == 0:
+        if len(tables_gt) == 1 and len(tables_prediction) == 0:
             # special case where the table from the ground truth was not detected by the prediction
-            polygon_content_gt = [x.bounding_box for x in tables_gt[0].cells]
-            polygon_content_prediction = []
-            return _intersection_over_union_polygon_region(polygon_content_gt, polygon_content_prediction)
-        table_gt: Table = tables_gt[0]
-        table_prediction: Table = tables_prediction[0]
-        polygon_content_gt = [x.bounding_box for x in table_gt.cells]
-        polygon_content_prediction = [x.bounding_box for x in table_prediction.cells]
-        return _intersection_over_union_polygon_region(polygon_content_gt, polygon_content_prediction)
+            return 0
+
+        matched_tables: Dict[Table, Table] = utility.match_tables(tables_gt, tables_prediction)
+        iou_total_value: int = 0
+        total_tables_viewed: int = 0
+        for table_gt, table_prediction in matched_tables.items():
+            cells_gt: List[Cell] = table_gt.cells
+            cells_prediction: List[Cell] = table_prediction.cells
+            polygon_content_gt = [x.bounding_box for x in cells_gt]
+            polygon_content_prediction = [x.bounding_box for x in cells_prediction]
+
+            iou_total_value += _intersection_over_union_polygon_region(polygon_content_gt, polygon_content_prediction)
+
+        return iou_total_value / total_tables_viewed
     else:
         raise RuntimeError("Wrong Arguments for intersection over union calculation. Please review the required args.")
 
@@ -94,6 +94,18 @@ def _iou_for_single_cell(cell: Cell, cells_to_search: List[Cell]) -> float:
         if search_cell_area.intersects(cell_area):
             intersection = search_cell_area.intersection(cell_area)
             return intersection.area / (search_cell_area.area + cell_area.area - intersection.area)
+
+    return 0
+
+
+def _iogt_for_single_cell(cell: Cell, cells_to_search: List[Cell]) -> float:
+    for search_cell in cells_to_search:
+        search_cell_area: Polygon = Polygon(search_cell.bounding_box.polygon)
+        cell_area: Polygon = Polygon(cell.bounding_box.polygon)
+
+        if search_cell_area.intersects(cell_area):
+            intersection = search_cell_area.intersection(cell_area)
+            return intersection.area / cell_area.area
 
     return 0
 
@@ -147,8 +159,3 @@ def cell_intersection_over_union(cell_1: PolygonRegion, cell_2: PolygonRegion):
         return intersection.area / (polygon_1.area + polygon_2.area - intersection.area)
     else:
         return 0
-
-# todo make version of this with single shared-file-format document where the ground truth is the index 0 element
-# shared-file-format version with ground truth as revision!
-# the other elements will be predictions -> it returns a list of prediction - for each version in comparison to the
-# ground truth
